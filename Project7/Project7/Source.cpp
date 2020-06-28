a895254f3cc4f3c856b4713b888f10b7083ad33b
khuzha <mr.khuzha@gmail.com>
2020-06-28 19:28:00 +0300

    remove printfs

--- Project7/Project7/Source.cpp
+++ Project7/Project7/Source.cpp
@@ -10,15 +10,15 @@ using namespace ::std;
 void PrintNegativeCycle(vector< pair<int, int> > shortestDistances, int vertex, int startVertex)
 {
 	if (vertex == startVertex) {
-		printf("%d ", vertex);
+		cout << "%d ", vertex;
 	}
 	else if (shortestDistances[vertex].second == 0) {
 		PrintNegativeCycle(shortestDistances, startVertex, startVertex);
-		printf("%d ", vertex);
+		cout << "%d ", vertex;
 	}
 	else {
 		PrintNegativeCycle(shortestDistances, shortestDistances[vertex].second, startVertex);
-		printf("%d ", vertex);
+		cout << "%d ", vertex;
 	}
 }
 
@@ -112,7 +112,7 @@ int main()
 	// pair.second -> edge's weight
 	vector< list< pair<int, int> > > adjacencyList(vertices + 1);
 
-	printf("Enter the Edges V1 -> V2, of weight W\n");
+	cout << "Enter the Edges V1 -> V2, of weight W\n";
 
 	for (int i = 1; i <= edges; ++i) {
 		cin >> v1 >> v2 >> weight;
@@ -121,18 +121,18 @@ int main()
 		adjacencyList[v1].push_back(make_pair(v2, weight));
 	}
 
-	printf("\nThe Adjacency List-\n");
+	cout << "\nThe Adjacency List-\n";
 	// Printing Adjacency List
 	for (int i = 1; i < adjacencyList.size(); ++i) {
-		printf("adjacencyList[%d] ", i);
+		cout << "adjacencyList[%d] ", i;
 
 		list< pair<int, int> >::iterator itr = adjacencyList[i].begin();
 
 		while (itr != adjacencyList[i].end()) {
-			printf(" -> %d(%d)", (*itr).first, (*itr).second);
+			cout << " -> %d(%d)", (*itr).first, (*itr).second;
 			++itr;
 		}
-		printf("\n");
+		cout << "\n";
 	}
 
 	vector< pair<int, int> > shortestDistances(vertices + 1);
@@ -142,16 +142,16 @@ int main()
 
 	int startVertex;
 
-	printf("\nEnter a Start Vertex -\n");
+	cout << "\nEnter a Start Vertex -\n";
 	cin >> startVertex;
 
 	int returnValue = bellmanFord(adjacencyList, vertices, startVertex, shortestDistances);
 
 	if (returnValue == -1) {
-		printf("No Negative Cycles exist in the Graph -\n");
+		cout << "No Negative Cycles exist in the Graph -\n";
 	}
 	else {
-		printf("Negative Cycles exists in the Graph -\n");
+		cout << "Negative Cycles exists in the Graph -\n";
 		// The Bellman-Ford Algorithm does not work with negative cycles,
 		// all it can do is merely detect them, so when a negative cycle
 		// is detected, the shortestDistances vector has wrong values
@@ -162,10 +162,10 @@ int main()
 		return 0;
 	}
 
-	printf("\n\nVertex    Shortest Distance to Vertex %d     Parent Vertex-\n", startVertex);
+	cout << "\n\nVertex    Shortest Distance to Vertex %d     Parent Vertex-\n", startVertex;
 	for (int i = 1; i <= vertices; ++i) {
-		printf("%d \t  %d \t\t\t\t    %d\n", i, shortestDistances[i].first,
-			shortestDistances[i].second);
+		cout << "%d \t  %d \t\t\t\t    %d\n", i, shortestDistances[i].first,
+			shortestDistances[i].second;
 	}
 
 	return 0;
