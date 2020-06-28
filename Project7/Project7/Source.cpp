162f17486a144c0bb686e6e9e7de2199dfeb7dc6
khuzha <mr.khuzha@gmail.com>
2020-06-28 19:44:27 +0300

    remove comments

--- Project7/Project7/Source.cpp
+++ Project7/Project7/Source.cpp
@@ -40,27 +40,18 @@ int bellmanFord(
 	shortestDistances[startVertex].first = 0;
 	shortestDistances[startVertex].second = 0;
 
-	// The Algorithm that computes Shortest Distances
-	for (i = 1; i <= vertices - 1; ++i) {    // Runs 'vertices - 1' times = O(|V|)
-		for (j = 1; j <= vertices; ++j) {    // Runs as many times as the edges = O(|E|)
-			// The code ahead basically explores the whole of Adjcency List,
-			// covering one edge once, so the runtime of the code in this 
-			// block is O(|E|)
-
+	for (i = 1; i <= vertices - 1; ++i) {
+		for (j = 1; j <= vertices; ++j) {
 			traverse = adjacencyList[j].begin();
 
 			while (traverse != adjacencyList[j].end()) {
 				if (shortestDistances[j].first == INT_MAX) {
-					// Important...!
-					//traverse = traverse->next;
 					++traverse;
 					continue;
 				}
 
-				// Checking for Relaxation
 				if ((*traverse).second + shortestDistances[j].first <
 					shortestDistances[(*traverse).first].first) {
-					// Relaxation
 					shortestDistances[(*traverse).first].first = (*traverse).second
 						+ shortestDistances[j].first;
 					shortestDistances[(*traverse).first].second = j;
@@ -71,15 +62,12 @@ int bellmanFord(
 		}
 	}
 
-	// Checking for Negative Cycles
 	for (j = 1; j <= vertices; ++j) {
 		traverse = adjacencyList[j].begin();
 
 		while (traverse != adjacencyList[j].end()) {
-			// Checking for further relaxation
 			if ((*traverse).second + shortestDistances[j].first <
 				shortestDistances[(*traverse).first].first) {
-				// Negative Cycle found as further relaxation is possible
 				return j;
 			}
 
@@ -100,10 +88,6 @@ int main()
 	cout << "Enter the Number of Edges -\n";
 	cin >> edges;
 
-	// Adjacency List is a vector of list.
-	// Where each element is a pair<int, int>
-	// pair.first -> the edge's destination
-	// pair.second -> edge's weight
 	vector< list< pair<int, int> > > adjacencyList(vertices + 1);
 
 	cout << "Enter the Edges V1 -> V2, of weight W\n";
@@ -111,12 +95,10 @@ int main()
 	for (int i = 1; i <= edges; ++i) {
 		cin >> v1 >> v2 >> weight;
 
-		// Adding Edge to the Directed Graph
 		adjacencyList[v1].push_back(make_pair(v2, weight));
 	}
 
 	cout << "\nThe Adjacency List-\n";
-	// Printing Adjacency List
 	for (int i = 1; i < adjacencyList.size(); ++i) {
 		cout << "adjacencyList[%d] ", i;
 
@@ -130,9 +112,6 @@ int main()
 	}
 
 	vector< pair<int, int> > shortestDistances(vertices + 1);
-	// shortestDistances is a vector of pairs
-	// pair.first -> the shortest distance from start vertex
-	// pair.second -> parent vertex in the shortest path
 
 	int startVertex;
 
@@ -146,9 +125,6 @@ int main()
 	}
 	else {
 		cout << "Negative Cycles exists in the Graph -\n";
-		// The Bellman-Ford Algorithm does not work with negative cycles,
-		// all it can do is merely detect them, so when a negative cycle
-		// is detected, the shortestDistances vector has wrong values
 
 		PrintNegativeCycle(shortestDistances, shortestDistances[returnValue].second
 			, returnValue);
